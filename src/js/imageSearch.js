f49f78900348e3f12165cc81f0250d307a4b2689
Vladimir35-Sashko <79024705+Vladimir35-Sashko@users.noreply.github.com>
2021-08-24 14:57:58 +0300

    loadmore btn

--- src/js/imageSearch.js
+++ src/js/imageSearch.js
@@ -29,12 +29,10 @@ function loadMoreBtnHandler() {
   servise.fetcArticles().then(hits => {
     const markup = buildListItemsTemplate(hits);
     iserListItems(markup);
-    window.scrollTo(0, 1000);
-
-    window.scrollTo({
-      top: 1000,
-      behavior: 'smooth',
-    });
+    loadMoreBtn.scrollIntoView({
+  behavior: 'smooth',
+  block: 'end',
+});
   });
 }
 function iserListItems(items) {
